307e8818650ecb65b65757e82fcc61f63b966bc9
sergio <sergiosanchezv20@gmail.com>
2021-09-24 17:04:42 -0400

    avances hasta finalizar section 4-views

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,12 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        'articles' => App\Article::take(3)->latest()->get()
+    ]);
+});
+
+Route::get('articles','ArticlesController@index');
+Route::get('/articles/{article}', 'ArticlesController@show');
